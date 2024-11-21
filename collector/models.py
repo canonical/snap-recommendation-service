8faeca4c8eaed06578c9e2471adf14aab9ddd380
Mohammad Iskandarany <M7md.k.isk@gmail.com>
2024-11-21 15:50:42 +0300

    chore(collector): add typing to model

--- collector/models.py
+++ collector/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Boolean, Integer, String, DateTime
-from sqlalchemy.orm import DeclarativeBase, mapped_column
+from datetime import datetime
+from typing import Optional, List
+from sqlalchemy import Boolean, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
@@ -7,24 +9,36 @@ class Base(DeclarativeBase):
 
 
 class Snap(Base):
-    __tablename__ = "snap"
-    snap_id = mapped_column(String, primary_key=True)
-    title = mapped_column(String)
-    name = mapped_column(String)
-    version = mapped_column(String)
-    summary = mapped_column(String)
-    description = mapped_column(String)
-    website = mapped_column(String, nullable=True)
-    contact = mapped_column(String, nullable=True)
-    publisher = mapped_column(String)
-    revision = mapped_column(Integer)  # Latest revision
-    links = mapped_column(String)
-    media = mapped_column(String)
-    developer_validation = mapped_column(String)
-    license = mapped_column(String)
-    last_updated = mapped_column(DateTime)
-    active_devices = mapped_column(Integer, default=0)
-    reaches_min_threshold = mapped_column(Boolean, default=False)
+    __tablename__: str = "snap"
+
+    snap_id: Mapped[str] = mapped_column(String, primary_key=True)
+    title: Mapped[str] = mapped_column(String)
+    name: Mapped[str] = mapped_column(String)
+    version: Mapped[str] = mapped_column(String)
+    summary: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String)
+    website: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    contact: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    publisher: Mapped[str] = mapped_column(String)
+    revision: Mapped[int] = mapped_column(Integer)  # Latest revision
+    links: Mapped[str] = mapped_column(String)
+    media: Mapped[str] = mapped_column(String)
+    developer_validation: Mapped[str] = mapped_column(String)
+    license: Mapped[str] = mapped_column(String)
+    last_updated: Mapped[datetime] = mapped_column(DateTime)
+    active_devices: Mapped[int] = mapped_column(Integer, default=0)
+    reaches_min_threshold: Mapped[bool] = mapped_column(Boolean, default=False)
 
 
-ALL_MEDIA_TYPES = ["icon", "screenshot", "video", "banner", "logo"]
+class Scores(Base):
+    __tablename__: str = "scores"
+
+    snap_id: Mapped[str] = mapped_column(
+        String, ForeignKey("snap.snap_id"), primary_key=True
+    )
+    popularity_score: Mapped[int] = mapped_column(Integer)
+    recency_score: Mapped[int] = mapped_column(Integer)
+    trending_score: Mapped[int] = mapped_column(Integer)
+
+
+ALL_MEDIA_TYPES: List[str] = ["icon", "screenshot", "video", "banner", "logo"]
