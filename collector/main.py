33c89b210b6efc70bf75e6ebeb43e4c1af3e3737
Mohammad Iskandarany <M7md.k.isk@gmail.com>
2024-11-26 11:42:52 +0300

    chore(collector): rename collect stage

--- collector/main.py
+++ collector/main.py
@@ -5,7 +5,7 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
 )
 
-from collector import collect_initial_snap_data
+from collect import collect_initial_snap_data
 from filter import filter_snaps_meeting_minimum_criteria
 from more_fields import fetch_extra_fields
 
