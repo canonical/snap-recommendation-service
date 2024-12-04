fb50347da80271e445cea25520efd326a76d0540
Mohammad Iskandarany <M7md.k.isk@gmail.com>
2024-12-04 16:30:28 +0300

    use postgres

--- collector/db.py
+++ collector/db.py
@@ -1,14 +1,20 @@
 import sqlalchemy
 import logging
 from models import Snap
+import os
 
 # create a new logger for the db module
 logger = logging.getLogger("db")
 
 
 def init_db():
+    connection_string = os.getenv("POSTGRESQL_DB_CONNECT_STRING")
+    if not connection_string:
+        logger.error("POSTGRESQL_DB_CONNECT_STRING is not set")
+        raise SystemExit(1)
+
     engine = sqlalchemy.create_engine(
-        "sqlite:///../db.sqlite", echo=logger.isEnabledFor(logging.DEBUG)
+        connection_string, echo=logger.isEnabledFor(logging.DEBUG)
     )
 
     Snap.metadata.create_all(engine)
