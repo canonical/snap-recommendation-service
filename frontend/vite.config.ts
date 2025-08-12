eabd54716c3d9ab69420f393299cd3657702bda4
Ilayda Cavusoglu Pars <ilayda.cavusoglupars@canonical.com>
2025-08-12 13:01:23 +0300

    chore: add React configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,28 +5,18 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   outDir: path.resolve(__dirname, '../snaprecommend/static'),
-  //   emptyOutDir: true,
-  //   rollupOptions: {
-  //     input: path.resolve(__dirname, 'index.html'),
-  //   }
-  // }
+  build: {
+    outDir: path.resolve(__dirname, '../snaprecommend/static'),
+    emptyOutDir: true,
+    assetsDir: '',
+  }, 
   server: {
     host: '0.0.0.0',
     proxy: {
-      '/_status': {
-        target: 'http://127.0.0.1:5000',
-      },
-      '/dashboard': {
-        target: 'http://127.0.0.1:5000',
-      },
-      '/api': {
-        target: 'http://127.0.0.1:5000',
-      },
-      '/login': {
-        target: 'http://127.0.0.1:5000',
-      },
+      '/_status': 'http://127.0.0.1:5000',
+      '/dashboard': 'http://127.0.0.1:5000',
+      '/api': 'http://127.0.0.1:5000',
+      '/login': 'http://127.0.0.1:5000'
     },
   },
 })
