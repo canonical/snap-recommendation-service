a26f81ca9fed96b1fc963f3a2d6eff9a81028ee8
Ilayda Cavusoglu Pars <ilayda.cavusoglupars@canonical.com>
2025-08-20 09:29:06 +0300

    feat: add layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import { Layout } from "./components";
 
 function App() {
   return (
-    <div>FE - Snap recommendations API - Copyright 2025 Canonical</div>
-  )
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/v2/dashboard/*" element={<Dashboard />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Layout/Layout.tsx
@@ -0,0 +1,43 @@
+import { ApplicationLayout, Col, Panel, Row } from "@canonical/react-components";
+import { Outlet, Link } from "react-router-dom";
+import { LayoutLogo } from "./LayoutLogo/LayoutLogo";
+
+export function Layout() {
+    return (
+        <ApplicationLayout
+            logo={{
+                name: "",
+                icon: "",
+                component: LayoutLogo,
+            }}
+            navItems={[{
+                items: [{
+                    icon: "information",
+                    component: Link,
+                    label: "Dashboard",
+                    to: "/v2/dashboard"
+                }, {
+                    icon: "delete",
+                    component: Link,
+                    label: "Excluded snaps",
+                    to: "/v2/dashboard/excluded_snaps"
+                }, {
+                    icon: "copy",
+                    component: Link,
+                    label: "Editorial Slices",
+                    to: "/v2/dashboard/editorial_slices"
+                }, {
+                    icon: "menu",
+                    component: Link,
+                    label: "Settings",
+                    to: "/v2/dashboard/settings"
+                }]
+            }]}  >
+            <Panel>
+                <Row>
+                    <Col size={12}> <Outlet /></Col>
+                </Row>
+            </Panel>
+        </ApplicationLayout>
+    );
+}
