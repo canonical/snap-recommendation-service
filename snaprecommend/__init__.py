1208641ed43bcf718d8f4bdccd167d97c6d328a8
Ilayda Cavusoglu Pars <ilayda.cavusoglupars@canonical.com>
2025-08-22 12:54:47 +0300

    chore: migrate dashboard to react

--- snaprecommend/__init__.py
+++ snaprecommend/__init__.py
@@ -23,6 +23,7 @@ OLD_PATHS = [
     "settings",
 ]
 
+
 def create_app(config_class=Config):
     app = Flask(__name__, static_folder='static', template_folder="templates")
     app.config.from_object(config_class)
