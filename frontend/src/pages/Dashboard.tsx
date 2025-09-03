2136d130fdbd7e972115e62fca4a497e82c83d2d
Mohammad Iskandarany <43501040+M7mdisk@users.noreply.github.com>
2025-09-03 13:22:17 +0300

    feat: integrate ratings (#75)
    * chore: add bindings
    
    * add fetching ratings to collector
    
    * add new top rated category, sync categories on migrate
    
    * add top_rated scoring
    
    * add top rated category to dashboard
    
    * score fixes
    
    * exclude generated from lint
    
    * fix docker-compose
    
    * ratings as env var

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,24 +3,27 @@ import { CategoryList } from "../components";
 
 export default function Dashboard() {
 
-    return (
-        <Panel title="Dashboard">
-            <div className={"u-fixed-width"}>
-                <h4 className="">Top snaps per category</h4>
-                <Row>
-                    <Col size={4}>
-                        <CategoryList category="popular" label="Most popular" />
-                    </Col>
+	return (
+		<Panel title="Dashboard">
+			<div className={"u-fixed-width"}>
+				<h4 className="">Top snaps per category</h4>
+				<Row>
+					<Col size={3}>
+						<CategoryList category="popular" label="Most popular" />
+					</Col>
 
-                    <Col size={4}>
-                        <CategoryList category="recent" label="Recently updated" />
-                    </Col>
+					<Col size={3}>
+						<CategoryList category="recent" label="Recently updated" />
+					</Col>
 
-                    <Col size={4}>
-                        <CategoryList category="trending" label="Trending" />
-                    </Col>
-                </Row>
-            </div>
-        </Panel>
-    )
+					<Col size={3}>
+						<CategoryList category="trending" label="Trending" />
+					</Col>
+					<Col size={3}>
+						<CategoryList category="top_rated" label="Top Rated" />
+					</Col>
+				</Row>
+			</div>
+		</Panel>
+	)
 }
