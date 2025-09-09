55894e0e269c3f664e6ae9bab99f9319f932097c
Ilayda Cavusoglu Pars <ilayda.cavusoglupars@canonical.com>
2025-09-09 14:01:53 +0300

    fix: lint error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => {
       host: '0.0.0.0',
       proxy: {
         '/': {
-          target: 'http://127.0.0.1:5000', // Flask backend
+          target: 'http://web:5000', // Flask backend
           changeOrigin: false,
           bypass: (req) => {
             // Let React handle /v2/dashboard and its subpaths
