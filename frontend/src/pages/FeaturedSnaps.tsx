50b5c399fa6c07a6b9690734fa5621425d1ea3e9
Ilayda Cavusoglu Pars <ilayda.cavusoglupars@canonical.com>
2025-09-26 14:48:09 +0300

    feat: add featured snap page

--- frontend/src/pages/FeaturedSnaps.tsx
+++ frontend/src/pages/FeaturedSnaps.tsx
@@ -1,12 +1,45 @@
+import { Col, Notification, Panel, Row } from "@canonical/react-components";
 import { useFetchData } from "../hooks/useFetchData";
 import type { FeaturedSnap } from "../types/snap";
-
+import { useEffect, useState } from "react";
+import { SortableCard } from "../components/SortableCard/SortableCard";
+import { LoadingCard } from "../components/LoadingCard/LoadingCard";
 
 export function FeaturedSnaps() {
     const { data, loading, error } = useFetchData<FeaturedSnap[]>('/featured');
-    return <div>
-        { loading ? "Loading...": "" }
-        { error ? "Error" : "" }
-        { data && JSON.stringify(data, null, 5) }
-    </div>
+
+    const [featuredSnaps, setFeaturedSnaps] = useState<FeaturedSnap[]>([]);
+
+    useEffect(() => {
+        if (data) {
+            setFeaturedSnaps(data)
+        }
+    }, [data])
+
+    return <Panel title="Featured Snaps">
+
+        <Row>
+            {
+                (error) && <Notification severity="negative" title="Error">
+                    {error}
+                </Notification>
+            }
+            {loading &&
+                [...Array(16)].map((_, index) => (
+                    <Col size={3} key={index}>
+                        <LoadingCard />
+                    </Col>
+                ))}
+            {featuredSnaps && (
+                <div>
+                    {featuredSnaps.map((snap) => (
+                        <SortableCard
+                            key={snap.package_name}
+                            snap={snap}
+                        />
+                    ))}
+                </div>
+            )}
+        </Row>
+    </Panel>
 }
