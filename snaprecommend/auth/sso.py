2f73f348b4b7594cdeccbf4d70e9ca2b18530700
Ilayda Cavusoglu Pars <ilayda.cavusoglupars@canonical.com>
2025-10-18 03:36:47 +0300

    Revert "feat: add macaroons - part 2"
    This reverts commit e0ad8af5cf98c5073c0bf87ace88deb58fe75aac.

--- snaprecommend/auth/sso.py
+++ snaprecommend/auth/sso.py
@@ -1,22 +1,15 @@
 import flask
 from django_openid_auth.teams import TeamsRequest, TeamsResponse
 from flask_openid import OpenID
-from snaprecommend.auth.macroon import MacaroonRequest, MacaroonResponse
 from snaprecommend.auth import authentication
-from snaprecommend.auth.session import publisher_gateway
-from snaprecommend.auth.constants import (
-    DEFAULT_SSO_TEAM,
-    LP_CANONICAL_TEAM,
-    LP_ADMIN_TEAM,
-    SSO_LOGIN_URL,
-)
+from snaprecommend.auth.constants import DEFAULT_SSO_TEAM, SSO_LOGIN_URL
 
 
 def init_sso(app: flask.Flask):
     open_id = OpenID(
         store_factory=lambda: None,
         safe_roots=[],
-        extension_responses=[MacaroonResponse, TeamsResponse],
+        extension_responses=[TeamsResponse],
     )
 
     SSO_TEAM = app.config.get("OPENID_LAUNCHPAD_TEAM", DEFAULT_SSO_TEAM)
@@ -30,11 +23,13 @@ def init_sso(app: flask.Flask):
     @open_id.loginhandler
     def login():
         if authentication.is_authenticated(flask.session):
+            if flask.request.is_secure:
+                return flask.redirect(
+                    open_id.get_next_url().replace("http://", "https://")
+                )
             return flask.redirect(open_id.get_next_url())
-        teams_request = TeamsRequest(
-            query_membership=[SSO_TEAM, LP_CANONICAL_TEAM, LP_ADMIN_TEAM]
-        )
 
+        teams_request = TeamsRequest(query_membership=[SSO_TEAM])
         return open_id.try_login(
             SSO_LOGIN_URL, ask_for=["email"], extensions=[teams_request]
         )
